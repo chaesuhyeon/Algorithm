f50135c0ab6819b4147b32534ddfc59548c02241
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2021-12-09 22:47:18 +0900

    FEAT : 10809

--- Algorithm_TIL/BAEKJOON/STEP7.py
+++ Algorithm_TIL/BAEKJOON/STEP7.py
@@ -24,7 +24,16 @@ print(result)
     
 
 # 문제 번호 10809 알파벳 찾기
-#
+# 알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+
+S = input()
+abc ='abcdefghijklmnopqrstuvwxyz'
+
+for i in abc:
+    if i in S:
+        print(S.index(i), end= ' ')
+    else:
+        print( -1, end =' ')
 
 # 문제 번호 2675 문자열 반복
 #
