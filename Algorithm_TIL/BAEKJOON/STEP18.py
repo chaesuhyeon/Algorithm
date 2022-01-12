a7203a26c68228c0bbaf5d0ec1bf44e5a58cf093
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-01-12 22:57:57 +0900

    FEAT : BOJ stack broze ~ sliver3

--- Algorithm_TIL/BAEKJOON/STEP18.py
+++ Algorithm_TIL/BAEKJOON/STEP18.py
@@ -399,37 +399,6 @@ else:
 
 
 # 문제 번호 14713 앵무새
-import sys
-bird = int(sys.stdin.readline().rstrip())
-stk = []
-
-status = True # 상태 체크를 위한 변수 
-for _ in range(bird): # 앵무새 수만큼 반복하여 앵무새가 말하는거 리스트에 저장 
-    stk.append(sys.stdin.readline().rstrip().split()) # 앵무새가 말한 단어? 문장?  [['i', 'want', 'to', 'see', 'you'], ['next', 'week'], ['good', 'luck']]
-
-word = sys.stdin.readline().split() # cseteram이 받아 적은 문장
-
-
-while word: # word가 존재하는 동안 
-    w = word.pop() # (i want next good luck week to see you) 에서 끝에 것부터 pop
-    
-    for j in range(bird): # 리스트 길이 만큼 반복 해줌 (0 ~ 2만큼)
-        if stk[j]: # stk[j]의 데이터가 존재하면 (처음에 이부분 안써줘서 런타임 오류(index error) 났음 )
-            if w == stk[j][-1]: # pop한 값과 2차원 배열 안의 끝에 값이 같으면
-                stk[j].pop() # 그 값을 pop
-                status = True # pop에 성공했으니 상태 True
-                break # 바로 위 반복문 종료 
-
-            else: # pop한 값이 2차원 배열 안의 끝값과 같지 않다면
-                status = False # 실패했으니 상태 False
-           
-
-    if status == False : # 반복이 끝났는데도 상태가 False이면 제거가 안됐다는 뜻 
-        print("Impossible") 
-        break # 바로 반복문 종료 (뒤에 더 계산할 필요x)
-
-if status == True : # 모든 반복문이 끝나고 상태가 True 라면 
-    print("Possible") # 성공 
 
 # 문제 번호 15815 천재수학자 성필 
 # 후위표기식2와 같은 방법으로 풀이함 
@@ -499,8 +468,6 @@ while stk: # 혹시 스택에 남아있는 문자가 있다면 (마지막이 문
  
 print(result) # 최종 결과 출력 
 
-    
-    
 
 
 # 문제 번호 17608 막대기
@@ -520,10 +487,74 @@ for i in range(len(num)): # 막대기 개수만큼 반복함
         stk.append(p) # stk에 추가해줌 
 print(len(stk)) # 스택의 길이 (막대기의 개수)
 
-# 문제 번호 17952 과제는 끝나지 않아!
     
 
+# 문제 번호 17952 과제는 끝나지 않아!
+import sys
+N = int(sys.stdin.readline().rstrip())
+stk = []
+sum = 0
+
+for _ in range(N):
+    hw= list(map(int, sys.stdin.readline().rstrip().split())) # 과제 정보 입력 
+
+    if hw[0] == 1: # 과제가 존재한다면 
+        stk.append([hw[1], hw[2]]) # 점수와 시간만 스택에 추가해줘서 2차원 리스트로 만듦
+        print("stk : " , stk)
+
+    if stk: # 숫자가 1이든 0이든 과제의 시간은 감소해야하기 때문에 이렇게 작성 / 스택이 존재한다면 ~ 
+        stk[-1][1] -= 1 # 시간 1분 감소 
+        print("stk time -1 : " , stk)
+        if stk[-1][1] == 0: # 시간이 0분이 되면 점수를 추가해주고 스택에서 빼줌 
+            sum += stk[-1][0] 
+            stk.pop()
+            print("stk pop : ", stk)
+        else:
+           pass
+    else:
+        pass           
+print(sum) # 최종적으로 더해진 점수 출력     
+
+
 # 문제 번호 23253 자료구조는 정말 최고야 
+# 막대기 문제랑 비슷한 것 같다.
+# 내림차순 아이디어 얻고 스택으로 다시 풀이 
+# 모든 더미가 내림차순으로 정렬이 되어 있는 상태여야함
+
+import sys
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+status = True # 상태 체크를 위한 변수 
+
+for _ in range(M):
+    stk = [] # 스택 초기화 
+    num = sys.stdin.readline() # 한 더미의 책 개수 
+    book= list(map(int, sys.stdin.readline().rstrip().split())) # 책 번호 입력 
+    
+    while book: # 책이 존재하는 동안 
+        for i in range(len(book)): # 책의 개수만큼 반복 
+            if stk: # 만약에 스택이 존재한다면 
+           
+                if stk[-1] < book[-1]: # 스택의 마지막 데이터와 책의 마지막 데이터를 비교했을 때 책의 번호가 더 크다면 pop 해줌 
+                    stk.append(book.pop()) # 스택에 append해줌
+                
+                else: # 만약에 스택에 있는 책 번호가 더 크다면 정렬실패 
+                    status = False # 실패했으니 상태 false
+                 
+            else: # 빈 스택이라면 데이터 추가 (맨처음에는 스택이 비어있기 때문에 데이터 넣어줘야함)
+                stk.append(book.pop())
+                
+        if status == False: # for문을 다 돌았는데 상태가 여전히 false라는 것은 정렬에 실패했다는 것
+            break # 반복문 더 진행할 필요가 없음(뒤에 숫자(책번호)가 더 남았을 경우에 해당)
+   
+    if status == False: # while문이 종료됐는데 상태가 false라면 내림차순이 안되는 부분이 있었다는 뜻
+        continue
+
+if status == True: # 반복문 다 끝났는데 false가 한부분도 없어서 다 true가 나왔다면 yes
+    print("Yes")
+else : # false가 나왔다는건 더미 중 내림차순이 아닌 더미가 있었다는 뜻이므로 no
+    print("No")
+
 
 
 
