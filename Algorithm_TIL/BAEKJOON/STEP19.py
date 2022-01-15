64c0804344f46f6968e9f815ffb22dc793bf8bf3
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-01-15 18:25:09 +0900

    FEAT : 11866

--- Algorithm_TIL/BAEKJOON/STEP19.py
+++ Algorithm_TIL/BAEKJOON/STEP19.py
@@ -75,7 +75,7 @@ for n in range(N):
             print(-1)
         else:
             print(queue[-1]) 
-            
+             
             
 # 문제 번호 2164 카드 2
 import sys
@@ -89,7 +89,25 @@ while len(queue) > 1: # 길이가 1이상인 경우만 반복
     queue.popleft() # 첫번째 원소 삭제 
 print(queue[0]) # 길이가 1개일때 그 1개의 원소를 출력                       
             
-            
+
+# 문제 번호 11866 요세푸스 문제 0
+from collections import deque
+N , K = map(int, input().split())
+q = deque([])
+
+for i in range(1,N+1):
+    q.append(i)   # 1 ~ 7까지 큐에 추가 
+
+print('<', end='')
+while q: # 큐가 존재하는 동안 
+    for i in range(K-1): # 두번째 숫자까지 추가하고 빼준다음에 
+        q.append(q[0])
+        q.popleft() 
+    print(q.popleft() , end='') # 세번째 숫자를 빼고 출력해줌 end를 써서 이어서 출력 
+    if q: # 큐가 존재한다면 ,를 써서 출력 형식 맞춰주기 
+        print(', ', end='')
+print('>')        # 큐가 다 pop돼서 while문이 종료되면 >출력해줌 
+
             
 
 
