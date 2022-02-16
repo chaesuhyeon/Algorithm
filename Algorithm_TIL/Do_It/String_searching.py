fdb92b4c748ee102f7f9a5a55de0c0434581364b
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-02-16 15:49:05 +0900

    FEAT : KMP법

--- Algorithm_TIL/Do_It/String_searching.py
+++ Algorithm_TIL/Do_It/String_searching.py
@@ -72,3 +72,49 @@ if __name__ == '__main__':
 # start가 지정되어 있으면 그 위치에서 판단을 시작하고, end가 지정되어 있으면 그 위치에서 비교를 중지함
 
 
+# KMP법
+# 브루트 포스법은 일치하지 않는 문자를 만나면 다시패턴의 첫 문자부터 검사를 수행하지만
+# KMP법은 검사한 결과를 효율적으로 사용할 수 있는 알고리즘
+
+def kmp_match(txt : str, pat: str) -> int :
+    """KMP법으로 문자열 검색"""
+    pt = 1 # txt를 따라가는 커서
+    pp = 0 # # pat를 따라가는 커서
+    skip = [0] * (len(pat) + 1) # 건너뛰기 표
+
+    # 건너뛰기표 만들기
+    skip[pt] = 0
+    while pt != len(pat):
+        if pat[pt] == pat[pp]:
+            pt += 1
+            pp += 1
+            skip[pt] == pp
+        elif pp == 0:
+            pt += 1
+            skip[pt] = pp
+        else:
+            pp = skip[pp]
+
+    # 문자열 검색하기
+    pt = pp = 0
+    while pt != len(txt) and pp != len(pat):
+        if txt[pt] == pat[pp]:
+            pt += 1
+            pp += 1
+        elif pp == 0:
+            pt += 1
+        else:
+            pp = skip[pp]
+    return pt - pp if pp == len(pat) else -1
+
+
+if __name__ == '__main__':
+    s1 = input('텍스트를 입력하세요 : ') # 텍스트용 문자열
+    s2 = input('패턴을 입력하세요 : ') # 패턴용 문자열
+
+    idx = kmp_match(s1,s2) # 문자열 s1 ~ s2까지를 KMP법으로 검색
+    if idx == -1:
+        print('텍스트 안에 패턴이 존재하지 않습니다.')
+    
+    else:
+        print(f'{idx+1}번째 문자가 일치합니다.')
