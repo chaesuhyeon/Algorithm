10cd8f6f318de2ccd18bf7ad44d216530ca4d05c
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-02-27 22:56:51 +0900

    FEAT : 14501 퇴사

--- Algorithm_TIL/BAEKJOON/문자열 검색.py
+++ Algorithm_TIL/BAEKJOON/문자열 검색.py
@@ -196,3 +196,21 @@ for i in range(1, n+1): # 부르트 포스법 --> 앞에서부터 탐색함 완�
         break
     if i == n: # i와 n이 같다는건 이미 실패함. 생성자 존재x (각 자리수를 더한 값도 포함이 되는데 이미 n과 같다는건 i에  각 자리수를 더했을때 값이 더 커짐 --> i는 생성자가 될 수 없음)
         print(0)
+
+# 14501 퇴사
+n = int(input())
+t = []
+p = []
+dp = []
+for i in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+dp.append(0) #뒤에 0을 추가해서 인덱스초과 오류 방지
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n: #데드라인이 기한을 넘어가는경우
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print(dp[0])   # 둘중에 더 큰값으로 dp[i]값을 갱신하고 최종적으로 dp[0]의 값이 최대가된다
