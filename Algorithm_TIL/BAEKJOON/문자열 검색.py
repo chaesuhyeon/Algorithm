5ff9d0a13400a2eace0e556d07e43530627b3bad
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-03-02 21:32:55 +0900

    FEAT : 17413 단어 뒤집기2

--- Algorithm_TIL/BAEKJOON/문자열 검색.py
+++ Algorithm_TIL/BAEKJOON/문자열 검색.py
@@ -214,3 +214,45 @@ for i in range(n - 1, -1, -1):
     else:
         dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
 print(dp[0])   # 둘중에 더 큰값으로 dp[i]값을 갱신하고 최종적으로 dp[0]의 값이 최대가된다
+
+# 17413 단어뒤집기2
+import sys
+from collections import deque
+
+stk = [] 
+q=deque()
+result = '' 
+state = True 
+
+S = sys.stdin.readline().rstrip()
+
+for i in S:
+    if i == "<": 
+        while stk:
+            result += stk.pop()  
+        q.append(i) 
+        state = False 
+    
+    elif i == ">": 
+        q.append(i) 
+        state = True 
+        while q : 
+            result += q.popleft()
+    
+    elif i==' ':
+        if state:
+            while stk:
+                result += stk.pop()
+            result += ' '  
+        else:
+            q.append(i) 
+            while q: 
+                result += q.popleft() #
+    else: 
+        if state: 
+            stk.append(i) 
+        else:
+            q.append(i) 
+while stk: 
+    result += stk.pop()
+print(result)
