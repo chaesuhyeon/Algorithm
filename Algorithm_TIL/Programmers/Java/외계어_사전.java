092ba0ecf215a6cad197a050ddf8e26f8405e12c
chaesuhyeon <ddd8177@gmail.com>
2022-11-28 22:12:08 +0900

    프로그래머스 - [외계어 사전]

---
+++ Algorithm_TIL/Programmers/Java/외계어_사전.java
@@ -0,0 +1,23 @@
+public class 외계어_사전 {
+    public static void main(String[] args) {
+        new 외계어_사전().solution(new String[]{"p", "o", "s"}, new String[]{"sod", "eocd", "qixm", "adio", "soo"});
+    }
+
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+
+        for(int i=0; i< dic.length; i++){
+            if(dic[i].length() == spell.length){
+                for(int j=0; j < spell.length; j++){
+                    dic[i] = dic[i].replaceFirst(spell[j],"");
+                    if(dic[i].equals("")){
+                        answer= 1;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
