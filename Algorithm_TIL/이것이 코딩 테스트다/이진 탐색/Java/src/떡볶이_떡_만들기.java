34aee41fda5595fc7776af6d61442cb0d7ffe58b
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-01-20 20:42:13 +0900

    이코테 > 떡볶이 떡 만들기

---
+++ Algorithm_TIL/이것이 코딩 테스트다/이진 탐색/Java/src/떡볶이_떡_만들기.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+// 백준 - 나무 자르기(https://www.acmicpc.net/problem/2805) 문제와 비슷 (돌려봤을 때 밑에 코드는 시간초과)
+public class 떡볶이_떡_만들기 {
+
+    public static void binarySearch(int[] arr, int sum,int target,int start, int end){
+        int mid = start + end / 2;
+
+        if(sum == target){
+            System.out.println(mid);
+        } else if(sum > target){
+            binarySearch(arr, 0, target,mid+1, end);
+        } else {
+            binarySearch(arr, 0, target,start, mid-1);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 떡의 개수(N)와 요청한 떡의 길이(M)
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // 각 떡의 개별 높이 정보
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 이진 탐색을 위한 시작점과 끝점 설정
+        int start = 0;
+        int end = (int) 1e9;
+        // 이진 탐색 수행 (반복적)
+        int result = 0;
+
+        while (start <= end) {
+            long total = 0;
+            int mid = (start + end) / 2;
+            for (int i = 0; i < n; i++) {
+                // 잘랐을 때의 떡의 양 계산
+                if (arr[i] > mid) total += arr[i] - mid;
+            }
+            if (total < m) { // 떡의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+                end = mid - 1;
+            }
+            else { // 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
+                result = mid; // 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
