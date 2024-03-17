dbac1a8e410e3d7d395e3fa6db024d96c5f8d0e5
suhyeon chae <ddd8177@gmail.com>
2024-03-17 21:13:22 +0900

    leetcode > 배열에 insert 위치 찾

---
+++ Algorithm_TIL/leetCode/Java/Search_Insert_Position.java
@@ -0,0 +1,23 @@
+public class Search_Insert_Position {
+
+    public static int searchInsert(int[] nums, int target) {
+        int result = 0;
+
+        for (int i = 0; i <nums.length; i++) {
+            if (nums[i] >= target) {
+                result = i;
+                break;
+            } else {
+                result = i+1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 5));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
+    }
+}
